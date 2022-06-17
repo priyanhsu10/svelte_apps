f8e1d873a87b0feb4eadf0efe85e1f6996d5927f
priyanshu10 <priyanshuparate@gmail.com>
2022-06-17 08:58:52 +0530

    condition statement

--- src/App.svelte
+++ src/App.svelte
@@ -1,42 +1,46 @@
 <script>
-  //   let firstname = "priyanshu";
-  //   let lastname = "parate";
-  //   $: fullname = `${firstname}  ${lastname}`;
-  //   $: {
-  //     console.log(firstname);
-  //     console.log(lastname);
-  //   }
-  //   const handleClick = () => {
-  //     if (toggle) {
-  //       color = "orrange";
-  //     } else {
-  //       color = "black";
-  //     }
-  //     toggle = !toggle;
-  //   };
-  //   const handleInput = (e) => {
-  //     color = e.target.value;
-  //   };
-  //   loops
+  import Model from "./Modal.svelte";
+  let firstname = "priyanshu";
+  let lastname = "parate";
+  let showModel = false;
+  $: fullname = `${firstname}  ${lastname}`;
+  $: {
+    console.log(firstname);
+    console.log(lastname);
+  }
+  const handleClick = () => {
+    if (toggle) {
+      color = "orrange";
+    } else {
+      color = "black";
+    }
+    toggle = !toggle;
+  };
+  const handleInput = (e) => {
+    color = e.target.value;
+  };
 
   let people = [
     { name: "yoshi", beltColour: "black", age: 25, id: 1 },
     { name: "mario", beltColour: "orange", age: 45, id: 2 },
     { name: "luigi", beltColour: "brown", age: 35, id: 3 },
   ];
-  const handleClick = (id) => {
+  const handleClick1 = (id) => {
     people = people.filter((x) => x.id != id);
   };
+  let num = 6;
 </script>
 
-<main>
-  <!-- <h1>Hello {fullname}</h1>
+<main class="main">
+  <button on:click={() => (showModel = !showModel)}>show </button>
+  <Model message="hi this message form message" list={people} {showModel} />
+  <h1>Hello {fullname}</h1>
   first name
   <input type="text" bind:value={firstname} />
   last name
   <input type="text" bind:value={lastname} />
 
-  <button on:click={handleClick}>change</button> -->
+  <button on:click={handleClick}>change</button>
   <!-- <input type="text" on:input={handleInput} value={color} /> -->
 
   {#each people as person (person.id)}
@@ -50,6 +54,14 @@
     <p>there is no data</p>
     <p>there is no data</p>
   {/each}
+
+  {#if num > 20}
+    <p>number is greater than 20</p>
+  {:else if num > 5}
+    <p>greater than 5</p>
+  {:else}
+    <p>Not greater than 5</p>
+  {/if}
 </main>
 
 <style>
---
+++ src/Modal.svelte
@@ -0,0 +1,41 @@
+<script>
+  export let message;
+  export let list = [];
+  export let showModel = false;
+  let ispromo = false;
+</script>
+
+{#if showModel}
+  <div class="backdrop" class:promo={ispromo}>
+    <div class="model">
+      <p>Sign upd for the offer</p>
+      <p>{message}</p>
+      <ul>
+        {#each list as item (item.id)}
+          <li>{item.name} ,{item.age}</li>
+        {/each}
+      </ul>
+    </div>
+  </div>
+{/if}
+
+<style>
+  .backdrop {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.8);
+  }
+  .model {
+    background: white;
+    width: 400px;
+    margin: 10% auto;
+    height: 200px;
+    text-align: center;
+    border-radius: 1rem;
+  }
+  .promo .model {
+    background: crimson;
+    color: white;
+  }
+</style>
